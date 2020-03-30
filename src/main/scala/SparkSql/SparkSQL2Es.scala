887c3de0ce01aece4031d0db03a790afa1a6a27e
yisheng.wu <yisheng.wu@vhall.com>
2020-03-30 17:41:32 +0800

    新增upsert写入es

--- src/main/scala/SparkSql/SparkSQL2Es.scala
+++ src/main/scala/SparkSql/SparkSQL2Es.scala
@@ -9,7 +9,7 @@ import org.apache.spark.sql.SparkSession
   * @Author yisheng.wu
   * @Date 2020/3/2617:12
   */
-case class Person(id: String, age: Integer, name: String, surname: String)
+case class Person(_id: String, age: Integer, name: String, surname: String)
 
 object SparkSQL2Es {
 
@@ -19,28 +19,64 @@ object SparkSQL2Es {
       .master("local[*]")
       .getOrCreate()
 
-    val orders = Seq(
-      Person("1025", 1025, "迪丽热巴", "迪丽热巴小姐姐"),
-      Person("1024", 1024, "迪丽热巴02", "迪丽热巴XXX"),
-      Person("1007", 1024, "热依扎", "热依扎")
-    )
+    val query= """{
+                 |	"_source": ["_id","age", "name"],
+                 |	"query": {
+                 |		"bool": {
+                 |			"filter": {
+                 |				"term": {
+                 |					"age": 1026
+                 |				}
+                 |			}
+                 |		}
+                 |	}
+                 |}""".stripMargin
 
-    import spark.implicits._
-
-    val ds = orders.toDF()
-
-    ds.write
+    val orders = spark.read
       .format("org.elasticsearch.spark.sql")
-      .option("es.mapping.id", "id")
+      .option("es.read.metadata", "true")
+      .option("es.nodes.wan.only","true")
+      .option("pushdown", "true")
+      .option("es.port","9200")
+      .option("es.net.ssl","false")
       .option("es.nodes", "192.168.1.171")
-      .option("es.port", "9200")
-      .option("es.update.script.inline",
-        "ctx._source.age = params.age;ctx._source.name = params.name;ctx._source.surname = params.surname")
-      .option("es.update.script.params", "age:age, name:name, surname:surname")
-      .option("es.write.operation", "upsert")
-      .option("es.batch.write.retry.count", 2)
-      .mode("append")
-      .save("test_new_client")
+      .option("query", query)
+//      .option("es.mapping.include", "age,name")
+//      .option("es.mapping.exclude", "surname")
+//      .option("es.read.source.filter", "age,name")w
+      .option("es.read.field.include", "age,name")
+      .option("es.read.field.exclude", "surname")
+      .option("es.read.metadata", false)  // 是否读取元数据字段
+//      .option("es.read.metadata.field", "_id")  // 元数据信息所在的字段
+      .load("test_new_client")
+
+//    orders.printSchema()
+//    orders.show(100)
+//
+//
+//    val orders = Seq(
+//      Person("1026", 21321321, "迪丽热巴", "迪丽热巴小姐姐"),
+//      Person("1027", 6785656, "迪丽热巴02", "迪丽热巴XXX"),
+//      Person("1028", 54634636, "热依扎", "热依扎")
+//    )
+//
+//  import spark.implicits._
+//
+//    val ds = orders.toDF()
+//
+//    ds.write
+//      .format("org.elasticsearch.spark.sql")
+//      .option("es.mapping.id", "_id")
+//      .option("es.nodes", "192.168.1.171")
+//      .option("es.port", "9200")
+//      .option("es.update.script.inline",
+//        "ctx._source.age = params.age")
+//      .option("es.update.script.params", "age:age")
+//      .option("es.write.operation", "upsert")
+//      .option("es.batch.write.retry.count", 2)
+////      .option("es.mapping.include", "age,name,surname")
+//      .mode("append")
+//      .save("test_new_client")
 
     spark.stop()
 
