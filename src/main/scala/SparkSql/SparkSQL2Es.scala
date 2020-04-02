babd7fbda0a91b2ddf7815078632eb00ae1bc90b
yisheng.wu <yisheng.wu@vhall.com>
2020-04-02 19:14:10 +0800

    新增数组更新的代码

--- src/main/scala/SparkSql/SparkSQL2Es.scala
+++ src/main/scala/SparkSql/SparkSQL2Es.scala
@@ -1,6 +1,6 @@
 package SparkSql
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{SparkSession}
 
 /**
   * @Title: SparkSQL2Es
@@ -32,54 +32,88 @@ object SparkSQL2Es {
                  |	}
                  |}""".stripMargin
 
-    val orders = spark.read
-      .format("org.elasticsearch.spark.sql")
-      .option("es.read.metadata", "true")
-      .option("es.nodes.wan.only","true")
-      .option("pushdown", "true")
-      .option("es.port","9200")
-      .option("es.net.ssl","false")
-      .option("es.nodes", "192.168.1.171")
-      .option("query", query)
-//      .option("es.mapping.include", "age,name")
-//      .option("es.mapping.exclude", "surname")
-//      .option("es.read.source.filter", "age,name")w
-      .option("es.read.field.include", "age,name")
-      .option("es.read.field.exclude", "surname")
-      .option("es.read.metadata", false)  // 是否读取元数据字段
-//      .option("es.read.metadata.field", "_id")  // 元数据信息所在的字段
-      .load("test_new_client")
+//    val orders = spark.read
+//      .format("org.elasticsearch.spark.sql")
+//      .option("es.read.metadata", "true")
+//      .option("es.nodes.wan.only","true")
+//      .option("pushdown", "true")
+//      .option("es.port","9200")
+//      .option("es.net.ssl","false")
+//      .option("es.nodes", "192.168.1.171")
+//      .option("query", query)
+////      .option("es.mapping.include", "age,name")
+////      .option("es.mapping.exclude", "surname")
+////      .option("es.read.source.filter", "age,name")w
+//      .option("es.read.field.include", "age,name")
+//      .option("es.read.field.exclude", "surname")
+//      .option("es.read.metadata", false)  // 是否读取元数据字段
+////      .option("es.read.metadata.field", "_id")  // 元数据信息所在的字段
+//      .load("test_new_client")
 
 //    orders.printSchema()
 //    orders.show(100)
-//
-//
-//    val orders = Seq(
-//      Person("1026", 21321321, "迪丽热巴", "迪丽热巴小姐姐"),
-//      Person("1027", 6785656, "迪丽热巴02", "迪丽热巴XXX"),
-//      Person("1028", 54634636, "热依扎", "热依扎")
-//    )
-//
-//  import spark.implicits._
-//
-//    val ds = orders.toDF()
-//
-//    ds.write
-//      .format("org.elasticsearch.spark.sql")
-//      .option("es.mapping.id", "_id")
-//      .option("es.nodes", "192.168.1.171")
-//      .option("es.port", "9200")
-//      .option("es.update.script.inline",
-//        "ctx._source.age = params.age")
-//      .option("es.update.script.params", "age:age")
-//      .option("es.write.operation", "upsert")
-//      .option("es.batch.write.retry.count", 2)
-////      .option("es.mapping.include", "age,name,surname")
-//      .mode("append")
-//      .save("test_new_client")
+
+
+    spark.udf.register("tag2arr", (id:String)=>tag2Arr(id))
+
+    val orders = Seq(
+      Person("1024", 21321321, "迪丽热巴", "迪丽热巴小姐姐"),
+      Person("1025", 6785656, "迪丽热巴02", "迪丽热巴XXX")
+    )
+
+    import spark.implicits._
+
+    val ds = orders.toDF()
+
+    ds.createOrReplaceTempView("orders")
+
+    val retds = spark.sql(
+      """select
+        | tag2arr(_id) as arr,
+        | _id,
+        | name,
+        | age,
+        | surname
+        |from
+        |orders
+      """.stripMargin)
+
+    retds.printSchema()
+
+    retds.show()
+
+    val script1 =
+      """ctx._source.age.clear();
+        |for(int i=0; i<params.arr.size();i++)
+        |{
+        | ctx._source.age.add(params.arr[i]);
+        |}
+        """.stripMargin.replaceAll("""[\\|\s*|\t|\r|\n]""", "")
+
+    val script2 =
+      """ctx._source.age=params.age;
+      """.stripMargin.replaceAll("""[\\|\s*|\t|\r|\n]""", "")
+
+    retds.write
+      .format("org.elasticsearch.spark.sql")
+      .option("es.mapping.id", "_id")
+      .option("es.nodes", "192.168.1.171")
+      .option("es.port", "9200")
+      .option("es.update.script.inline",script2)
+      .option("es.update.script.lang","painless")
+      .option("es.update.script.params", "age:arr")
+      .option("es.write.operation", "upsert")
+      .option("es.batch.write.retry.count", 2)
+//      .option("es.mapping.include", "age,name,surname")
+      .mode("append")
+      .save("test_new_client")
 
     spark.stop()
 
+  }
+
+  private def tag2Arr(id: String): Seq[Long] ={
+    Seq(10004,10005, 10006)
   }
 
 }
